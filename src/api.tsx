802cb54e833db6b6cb2d1e197e4ad4749ca3b3bb
OldsoulPhil <brenenshea@gmail.com>
2023-09-13 15:32:02 -0400

    second committ for this project

--- src/api.tsx
+++ src/api.tsx
@@ -1,16 +1,44 @@
-const getAllDogs = () => {
-  // fill out method
+import { Dog } from "./types";
+
+export const baseUrl = "http://localhost:3000";
+
+const getAllDogs = () =>
+  fetch(`${baseUrl}/dogs`).then((res): Promise<Dog[]> => res.json());
+
+const postDog = (dog: Omit<Dog, "id">): Promise<Dog> => {
+  return fetch(`${baseUrl}/dogs`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dog),
+  }).then((res): Promise<Dog> => res.json());
 };
 
-const postDog = () => {
-  // fill out method
-};
-const deleteDogRequest = () => {
-  // fill out method
+const deleteDogRequest = (id: number) => {
+  return fetch(`${baseUrl}/dogs/${id}`, {
+    method: "DELETE",
+  }).then((res) => {
+    if (!res.ok) {
+      throw new Error("Couldn't delete dog");
+    } else return;
+  });
 };
 
-const patchFavoriteForDog = () => {
-  // fill out method
+const patchFavoriteForDog = (patch: boolean, id: number) => {
+  return fetch(`${baseUrl}/dogs/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      isFavorite: patch,
+    }),
+  }).then((res) => {
+    if (!res.ok) {
+      throw new Error("Dog didn't patch");
+    } else return;
+  });
 };
 
 export const Requests = {
